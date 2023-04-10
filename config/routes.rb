8f84dadc1e368542ace5001e71a2a221614d2839
EC2 Default User <ec2-user@ip-172-31-9-218.ap-northeast-1.compute.internal>
2023-04-10 11:30:01 +0000

    4章

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+
+  # .../lists/1 や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show' , as: 'list'
+end
